0d880f082ba640fbf3dabe8ec9c103674eb2fd12
Matthew Vogee <mvogee@yahoo.com>
2021-12-28 15:43:35 -0800

    messages display on client side as they come in from server.

--- app/public/js/socketConnect.js
+++ app/public/js/socketConnect.js
@@ -1,14 +1,63 @@
 var socket = io();
+let messageBoard = document.querySelector('#messageBoard');
+let form = document.querySelector('#postForm');
+let input = document.querySelector('#postInput');
+
+function createPostElement(postObj) {
+    // create wrapper div
+    postDiv = document.createElement("div");
+    postDiv.className = "post-wrapper";
+    // create username element
+    userNameNode = document.createElement("h3");
+    userNameNode.className = "post-username";
+    userNameNode.textContent = postObj.user
+    // create body text element
+    textNode = document.createElement("p");
+    textNode.className = "post-body";
+    textNode.textContent = postObj.text;
+    // create timeStamp element
+    timeStampNode = document.createElement("p");
+    timeStampNode.className = "post-timestamp";
+    timeStampNode.textContent = postObj.timeStamp.toString();
+
+    postDiv.appendChild(userNameNode);
+    postDiv.appendChild(textNode);
+    postDiv.appendChild(timeStampNode);
+
+    return postDiv;
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    if (input.value != "") {
+        socket.emit("messageFromClient", {
+            user: "username",
+            message: input.value,
+            timestamp: new(Date)
+        });
+        input.value = "";
+    }
+});
 
 socket.on("connect", () => {
     console.log("connected to the server");
 });
 
-socket.on("newMessage", (message) => {
-    console.log("newMessage", message);
+socket.on("messageFromServer", (data) => {
+    console.log("messageFromServer", data);
+    if (Array.isArray(data)) {
+        data.forEach((obj) => {
+            messageBoard.appendChild(createPostElement(obj));
+            window.scrollTo(0, document.body.scrollHeight);
+        });
+    }
+    else if (typeof data === "object"){
+        messageBoard.appendChild(createPostElement(data));
+        window.scrollTo(0, document.body.scrollHeight);
+    }
 });
 
-socket.emit("createMessage", {
+socket.emit("messageFromClient", {
     to: "server@server",
     text: "message to server from user"
 });
