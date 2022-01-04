a4976d2bdf8449d18880333b1cf3e9e09202a8a5
Matthew Vogee <matthewvogee@Matthews-MacBook-Pro.local>
2022-01-03 19:51:04 -0800

    encrypting password and adding user to database if no existing username works

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,7 +2,8 @@ const path = require('path');
 require('dotenv').config({ path: path.resolve(path.join(__dirname, '../.env'))});
 const mongoose = require('mongoose');
 const {connectmongoose, User, Post} = require(path.resolve(path.join(__dirname, '../db.js')));
-
+const bcrypt = require("bcrypt");
+const saltRounds =  10;
 var router = require('express').Router();
 
 connectmongoose(mongoose, process.env.DB_ROUTE);
@@ -33,11 +34,42 @@ router.post("/signup", (req, res) => {
     console.log(req.body.username);
     console.log(req.body.password);
     // query database here. if username found return false object
+    User.find({userName: req.body.username}, (err, result) => {
+        if (err) {
+            console.log("an error occurred: ", err);
+        }
+        if (result.length === 0) {
+            console.log("no result username available");
+            bcrypt.hash(req.body.password, saltRounds, (err, encrypted) => {
+                if (err) {
+                    console.log("error occurred encrypting password: ", err);
+                }
+                let userData = {
+                    userName: req.body.username,
+                    email: req.body.email ? req.body.email : null,
+                    password: encrypted,
+                    created: new Date,
+                }
+                User.create(userData, (err, result) => {
+                    if (err) {
+                        console.log("error occurred adding user to database. ", err);
+                    }
+                    console.log(result);
+                    // here is where you would send back a user using
+                    res.json(result);
+                });
+            });
+        }
+        else {
+            res.json({
+                status: false,
+                reason: "username already used"
+            });
+        }
+        console.log(result);
+    });
     // otherwise enter new user into database. encrypt password and email.
     // decryption here
-    res.json({
-        status: false,
-        reason: "none of yo business"
-    });
+    
 });
 module.exports = router;
